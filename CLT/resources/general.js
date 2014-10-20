3dbe018b7ef5507fd29b843024f4d0b24a433daf
voyager2718 <voyager2718@gmail.com>
2014-10-20 21:43:50 +0200

    +IE11

--- CLT/resources/general.js
+++ CLT/resources/general.js
@@ -2,18 +2,13 @@ function getBrowserVersion() {
     var browser = {};
     var userAgent = navigator.userAgent.toLowerCase();
     var s;
-    (s = userAgent.match(/msie ([\d.]+)/))
-           ? browser.ie = s[1]
-            : (s = userAgent.match(/firefox\/([\d.]+)/))
-                    ? browser.firefox = s[1]
-                    : (s = userAgent.match(/chrome\/([\d.]+)/))
-                            ? browser.chrome = s[1]
-                           : (s = userAgent.match(/opera.([\d.]+)/))
-                                    ? browser.opera = s[1]
-                                    : (s = userAgent
-                                            .match(/version\/([\d.]+).*safari/))
-                                            ? browser.safari = s[1]
-                                            : 0;
+    (s = userAgent.match(/rv:([\d.]+)\) like gecko/)) ? browser.ie = s[1] :
+    (s = userAgent.match(/msie ([\d.]+)/)) ? browser.ie = s[1] :
+    (s = userAgent.match(/firefox\/([\d.]+)/)) ? browser.firefox = s[1] :
+    (s = userAgent.match(/chrome\/([\d.]+)/)) ? browser.chrome = s[1] :
+    (s = userAgent.match(/opera.([\d.]+)/)) ? browser.opera = s[1] :
+    (s = userAgent.match(/version\/([\d.]+).*safari/)) ? browser.safari = s[1] :
+    0;
     var version = "";
     if (browser.ie) {
         version = 'msie ' + browser.ie;
