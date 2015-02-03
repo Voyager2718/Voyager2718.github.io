ed5977e6e41e39212d1a209a141538fc3022b10e
Voyager2718 <voyager2718@gmail.com>
2015-02-03 22:27:46 +0100

    修复按退出键的错误

--- MyBlogger/resources/login.js
+++ MyBlogger/resources/login.js
@@ -3,20 +3,7 @@ if (window.innerWidth > 1500)
     document.getElementById('menu').style.width = "60%";
 var fps = 0;
 var timerID;
-
-/*
-function show_login_div() {
-    if (fps >= 20) {
-        clearTimeout(timerID);
-    }
-    else {
-        var login_div = document.getElementById("login_div");
-        login_div.style.marginTop = 9 + fps + "px";
-        login_div.style.opacity = fps / 20;
-        fps++;
-    }
-}
-*/
+document.onkeyup = key;
 
 function show_login_div() {
     var login_div = document.getElementById("login_div");
@@ -25,6 +12,7 @@ function show_login_div() {
     login_div.style.display = "block";
     login_eff.style.display = "block";
     if (fps >= 20) {
+        document.getElementById("username").focus();
         clearTimeout(timerID);
     }
     else {
@@ -60,12 +48,9 @@ function login_form() {
     var cover = document.getElementById("cover");
     var window_h = document.body.scrollHeight;
     if (login_display == false) {
-        //cover.setAttribute("style", "-webkit-filter:blur(20px);");
-        //login_div.style.display = "block";
-        //login_eff.style.display = "block";
         login_eff.style.height = window_h + "px";
         login_display = true;
-        timerID = setInterval(show_login_div, 15);
+        timerID = setInterval(show_login_div, 10);
     }
     else {
         login_display = false;
@@ -73,9 +58,10 @@ function login_form() {
     }
 }
 
-function key() {
-    if (login_display == true) {
+function key(e) {
+    var kc = e.keyCode;
+    if (login_display == true && kc == 27) {
         login_display = false;
-        timerID = setInterval(close_login_div, 10);
+        timerID = setInterval(close_login_div, 15);
     }
 }
