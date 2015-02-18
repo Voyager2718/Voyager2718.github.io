931cefed19ce26fdedf06d45d611cfba44ef43f4
Voyager2718 <voyager2718@gmail.com>
2015-02-18 22:27:38 +0100

    增加Ajax和PHP

---
+++ 誓言录/act.php
@@ -0,0 +1,5 @@
+<?php
+	$pmsID = $_POST["pmsID"];
+	$password = 456;
+	echo "<div id=\"idBox\">".$pmsID.$password."</div>";
+?>
---
+++ 誓言录/resources/index.js
@@ -0,0 +1,29 @@
+﻿function key(e) {
+    if (e.keyCode == 27)
+        document.getElementById('Admin').style.display = 'none';
+    if (e.keyCode == 82 && e.altKey)
+        document.getElementById('Admin').style.display = 'block';
+}
+
+function getPMS() {
+    var pw = document.getElementById("pw").value;
+    var pmsID = document.getElementById("pmsID").value;
+    if (pw == "" || pmsID == "")
+        alert("Please fill in all spaces.");
+    else {
+        var xmlhttp;
+        if (window.XMLHttpRequest)
+            xmlhttp = new XMLHttpRequest();
+        else if (window.ActiveXObject)
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        else
+            throw new Error("Your browser doesn't support Ajax");
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.status == 200 && xmlhttp.readyState == 4)
+                document.getElementById("PMSShow").innerHTML = xmlhttp.responseText;
+        }
+        xmlhttp.open("POST", "act.php", true);
+        xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xmlhttp.send("pmsID=" + pmsID + "&password=" + pw + "&type=query");
+    }
+}
