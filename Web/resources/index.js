ca00f3718523a865d34f34e87e6ab773c0e43c93
Voyager2718 <voyager2718@gmail.com>
2016-01-14 23:11:02 +0100

    Fixed

--- Web/resources/index.js
+++ Web/resources/index.js
@@ -1,14 +1,14 @@
+var zoom = 100;
 //Init
 $(document).ready(function () {
     //Device detect
-    var zoom = 100;
     if (device.mobile()) {
-        $("body").css("font-size", "300%");
-        zoom = 300;
+        $("body").css("font-size", "200%");
+        zoom = 200;
     }
     else if (device.tablet()) {
-        $("body").css("font-size", "400%");
-        zoom = 400;
+        $("body").css("font-size", "300%");
+        zoom = 300;
     }
     $("#cv").text(lang.WebCV);
     $("#cardTitle0").text(lang.PersonalInfo);
@@ -38,8 +38,8 @@ $(window).resize(function () {
 //Functions
 function setPositions() {
     $("#canvas").css("left", ($(window).width() - 900) / 2 + "px");
-    if (device.landscape())
-        $("body").css("font-size", "400%");
+    if (!device.landscape())
+        $("body").css("font-size", 1.5 * zoom + "%");
     else
         $("body").css("font-size", zoom + "%");
 }
