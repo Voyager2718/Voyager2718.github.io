f1c0613b81c261db90bb750de2a639201f011631
Voyager2718 <voyager2718@gmail.com>
2016-03-18 14:11:48 +0100

    fixed a small bug

--- CV/Web2/resources/index.js
+++ CV/Web2/resources/index.js
@@ -6,17 +6,18 @@ function setSwitchPages() {
 }
 
 function switchPages() {
-    p = p < 8 ? p + 1 : 0;
+    p = p < 9 ? p + 1 : 0;
     switch (p) {
         case 0: self.location.href = "#1st"; break;
         case 1: self.location.href = "#2nd"; break;
         case 2: self.location.href = "#2nd/1"; break;
         case 3: self.location.href = "#3rd"; break;
-        case 4: self.location.href = "#4th/1"; break;
-        case 5: self.location.href = "#4th/2"; break;
-        case 6: self.location.href = "#4th/3"; break;
-        case 7: self.location.href = "#4th"; break;
+        case 4: self.location.href = "#4th"; break;
+        case 5: self.location.href = "#4th/1"; break;
+        case 6: self.location.href = "#4th/2"; break;
+        case 7: self.location.href = "#4th/3"; break;
         case 8: self.location.href = "#5th"; break;
+        case 9: self.location.href = "#6th"; break;
     }
 }
 
