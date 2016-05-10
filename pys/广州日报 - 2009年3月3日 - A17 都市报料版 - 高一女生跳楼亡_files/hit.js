8d0e75bf7b3c9c503180b78b0bf7fa7b27ef711e
Voyager2718 <voyager2718@gmail.com>
2016-05-10 13:36:10 +0200

    删除无无聊聊的东西

--- pys/广州日报 - 2009年3月3日 - A17 都市报料版 - 高一女生跳楼亡_files/hit.js
+++
@@ -1,9 +0,0 @@
-function dohit(){		
-		if (document.readyState == 'complete' )
-    	{		
-		document.frames("hitClickIframe").document.hitform.url.value = location.href;
-		document.frames("hitClickIframe").document.hitform.submit();
-		document.hitClickIframe.style.display="none";
-		}else
-		window.setTimeout("dohit()","300"); 	
-}
